3e4c9766b42ceaf6facf30fb821105b36f186c13
veevax <vigouroux.arnaud@gmail.com>
2017-03-02 11:15:28 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# My project's README
+
+The idea of this project is to build a middle-layer tool that runs on a Heroku.
+
+The tool will receive POST requests from a wordpress website and post it on a Slack channel.
+
+The tools is built using the Flask framework and the `requests` library.
---
+++ message-app.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify
+from flask import abort
+from flask import make_response
+from flask import request
+from flask import url_for
+import requests
+import os 
+
+slackurl = os.environ['SLACK_API_KEY']
+
+app = Flask(__name__)
+
+@app.route('/wptoslack/api/v1.0/message', methods=['POST'])
+def create_task():
+    headers = {'Content-type': 'application/json'}
+
+    author = str(request.form.get("comment_author"))
+    content = str(request.form.get("comment_content"))
+    date = str(request.form.get("comment_date"))
+
+    data_message = '"username": "wordpresshelper","text":"New comment needs your approval!","icon_emoji": ":squirrel:"'
+    attachments = '[{"author_name": "'+ author +'", "text": "' + content + '", "title": "Posted on ' + date + '"}]'
+
+    r = requests.post(slackurl, data='{'+ data_message +', "attachments": ' + attachments + '}', headers=headers)
+
+    return jsonify({'response': "Nice!"}), 201
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
